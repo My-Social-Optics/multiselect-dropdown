638ab16c8954ca59c53350d8f104e849c7f32d17
Reyhan Javier <reyhan.qartem@gmail.com>
2024-02-28 12:48:53 +0700

    Update icons.dart

---
+++ lib/widgets/icons.dart
@@ -0,0 +1,208 @@
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+
+/// {@template so_icons}
+///
+/// All icons available.
+///
+/// ```dart
+/// SOIcon(
+///   SOIcons.sidebarToggle,
+///   color: SOColors.basePrimary,
+///   size: 32,
+/// )
+/// ```
+///
+/// {@endtemplate}
+class SOIcons {
+  /// Sidebar toggle
+  static const sidebarToggle = LucideIcons.panelLeftOpen;
+
+  /// Profile button icon
+  static const profileButton = LucideIcons.chevronsUpDown;
+
+  /// Notification icon
+  static const notification = LucideIcons.bell;
+
+  /// Setting icon
+  static const setting = LucideIcons.settings;
+
+  /// Dashboard icon
+  static const dashboard = LucideIcons.layoutDashboard;
+
+  /// Courses icon
+  static const courses = LucideIcons.presentation;
+
+  /// Resources icon
+  static const resources = LucideIcons.bookText;
+
+  /// Support center icon
+  static const supportCenter = LucideIcons.headphones;
+
+  /// User icon
+  static const user = LucideIcons.usersRound;
+
+  /// Graduation cap icon
+  static const graduationCap = LucideIcons.graduationCap;
+
+  /// Arrow up
+  static const arrowUp = LucideIcons.chevronUp;
+
+  /// Arrow down
+  static const arrowDown = LucideIcons.chevronDown;
+
+  /// Arrow right
+  static const arrowRight = LucideIcons.chevronRight;
+
+  /// Success
+  static const success = LucideIcons.checkCircle2;
+
+  /// Error
+  static const error = LucideIcons.xCircle;
+
+  /// Info
+  static const info = LucideIcons.info;
+
+  /// Warning
+  static const warning = LucideIcons.alertTriangle;
+
+  /// Arrow key left
+  static const arrowKeyLeft = LucideIcons.arrowLeft;
+
+  /// Arrow key right
+  static const arrowKeyRight = LucideIcons.arrowRight;
+
+  /// Arrow key up
+  static const arrowKeyUp = LucideIcons.arrowUp;
+
+  /// Arrow key Up Down
+  static const arrowUpDown = LucideIcons.arrowUpDown;
+
+  /// Default placeholder icon
+  static const placeholder = LucideIcons.fileQuestion;
+
+  /// Plus (+)
+  static const plus = LucideIcons.plus;
+
+  /// Minus (-)
+  static const minus = LucideIcons.minus;
+
+  /// Clear (x)
+  static const clear = LucideIcons.x;
+
+  /// Check (v)
+  static const check = LucideIcons.check;
+
+  /// Search magnifying glass
+  static const search = LucideIcons.search;
+
+  /// Filter funnel
+  static const filter = LucideIcons.filter;
+
+  /// More triple dot horizontal
+  static const moreHorizontal = LucideIcons.moreHorizontal;
+
+  /// More triple dot vertical
+  static const moreVertical = LucideIcons.moreVertical;
+
+  /// Log out icon
+  static const person = LucideIcons.user;
+
+  /// square user round
+  static const squareUserRound = LucideIcons.squareUserRound;
+
+  /// Mails
+  static const mails = LucideIcons.mails;
+
+  /// Log out icon
+  static const logout = LucideIcons.logOut;
+
+  /// Trash icon
+  static const trash2 = LucideIcons.trash2;
+
+  /// Edit icon
+  static const pencil = LucideIcons.pencil;
+
+  /// Full screen icon
+  static const fullscreen = LucideIcons.fullscreen;
+
+  /// Email icon
+  static const email = LucideIcons.mail;
+
+  /// Country icon
+  static const country = LucideIcons.flag;
+
+  /// State icon
+  static const state = LucideIcons.mapPin;
+
+  /// Date joined icon
+  static const dateJoined = LucideIcons.doorOpen;
+
+  /// Last login icon
+  static const lastLogin = LucideIcons.logIn;
+
+  /// School building icon
+  static const schools = LucideIcons.school;
+
+  /// districts icon
+  static const districts = LucideIcons.mapPinned;
+
+  /// Classes icon
+  static const classes = LucideIcons.doorClosed;
+
+  /// Map
+  static const map = LucideIcons.map;
+
+  /// Door Closed
+  static const doorClosed = LucideIcons.doorClosed;
+
+  /// Door Closed
+  static const contact2 = LucideIcons.contact2;
+
+  /// X
+  static const x = LucideIcons.x;
+
+  /// School
+  static const school = LucideIcons.school;
+
+  /// Block
+  static const circleSlash2 = LucideIcons.circleSlash2;
+
+  /// User cog
+  static const userCog = LucideIcons.userCog;
+
+  /// Calendar
+  static const calendar = LucideIcons.calendar;
+
+  /// Hash
+  static const hash = LucideIcons.hash;
+
+  /// Hand
+  static const hand = LucideIcons.hand;
+
+  /// Bell dot
+  static const bellDot = LucideIcons.bellDot;
+
+  /// Arrow Up Down
+  static const sort = LucideIcons.arrowUpDown;
+
+  /// Square user
+  static const squareUser = LucideIcons.squareUser;
+
+  /// File Up
+  static const fileUp = LucideIcons.fileUp;
+
+  /// File Text
+  static const fileText = LucideIcons.fileText;
+
+  /// Thumbs up
+  static const thumbsUp = LucideIcons.thumbsUp;
+
+  /// Lock
+  static const lock = LucideIcons.lock;
+
+  /// Eye
+  static const eye = LucideIcons.eye;
+
+  /// Eye off
+  static const eyeOff = LucideIcons.eyeOff;
+}
